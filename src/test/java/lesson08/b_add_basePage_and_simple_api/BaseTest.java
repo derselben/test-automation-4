20a9716ec68f8a5cdac65c2a036eb6dd320ce647
derselben <derselben@gmail.com>
2019-03-19 14:26:54 +0200

    lesson09 classwork

--- src/test/java/lesson08/b_add_basePage_and_simple_api/BaseTest.java
+++ src/test/java/lesson08/b_add_basePage_and_simple_api/BaseTest.java
@@ -9,7 +9,6 @@ import org.junit.runner.Description;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
