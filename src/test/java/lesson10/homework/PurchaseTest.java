59b5f5aba4bc1cd912c32f63144bd83af7a2feab
Pavlo.Babynin <pavlo.babynin@infopulse.com>
2019-03-31 14:09:17 +0300

    lesson11 homework - small renames master

--- src/test/java/lesson10/homework/PurchaseTest.java
+++ src/test/java/lesson10/homework/PurchaseTest.java
@@ -32,7 +32,7 @@ public class PurchaseTest extends BaseTest {
     }
 
     @Test
-    public void verifyOrder(){
+    public void verifyThatOrderIsSameAsWasCheckedOut(){
 
         landingPage
                 .searchFor("Dress")
