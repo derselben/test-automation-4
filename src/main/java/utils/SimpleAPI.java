5220df5c12d68e30aeeb99f19dd63fdd56cee484
derselben <derselben@gmail.com>
2019-04-02 14:16:41 +0300

    lesson13 classwork

--- src/main/java/utils/SimpleAPI.java
+++ src/main/java/utils/SimpleAPI.java
@@ -1,17 +1,22 @@
 package utils;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.function.Function;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfAllElementsLocatedBy;
 
 public abstract class SimpleAPI {
+
+	private static final Logger LOGGER = LogManager.getLogger(SimpleAPI.class);
 
 	protected abstract WebDriver getDriver();
 
@@ -45,6 +50,19 @@ public abstract class SimpleAPI {
 
 	protected <T> T waitFor(ExpectedCondition<T> condition, long timeout) {
 		return (new WebDriverWait(getDriver(), timeout)).until(condition);
+	}
+
+	protected void captureScreenshoot(String methodName) {
+		File screenshot = ((TakesScreenshot)getDriver())
+				.getScreenshotAs(OutputType.FILE);
+		String screenshotName = screenshot.getName().replace("screenshot", methodName + "_");
+		String path = System.getProperty("report.path") + "/screenshots/" + screenshotName;
+		try {
+			FileUtils.copyFile(screenshot, new File(path));
+			LOGGER.error("Screenshot was got: " + screenshotName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	protected <T> T waitFor(ExpectedCondition<T> condition) {
